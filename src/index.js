58db02aff8b6b09f47830c29c80c5a518fcd3971
Love Jindal <lkjindal123@gmail.com>
2026-07-15 19:57:46 +0530

    Setup of smtp using nodemailer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig');
 
+const {sendBasicEmail} = require('./services/email-service');
+
 const setupAndStartServer = () => {
     const app = express();
     app.use(bodyParser.json());
@@ -10,7 +12,15 @@ const setupAndStartServer = () => {
 
     app.listen(PORT, () =>{
         console.log(`Server started!`)
+
+        sendBasicEmail(
+            'lkjindai123@gmail.com',
+            '24136@iiitu.ac.in',
+            'This is a test email',
+            'Hey, how are you, I hope you like the support'
+        )
     })
+
 }
 
 setupAndStartServer();
