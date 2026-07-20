a8fef291d4c901b508750430369c1c4bfafb06c9
Love Jindal <lkjindal123@gmail.com>
2026-07-21 01:30:37 +0530

    Added rabbitMQ basic

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -5,5 +5,8 @@ dotenv.config();
 module.exports = {
     PORT: process.env.PORT,
     EMAIL_ID: process.env.EMAIL_ID,
-    EMAIL_PASS: process.env.EMAIL_PASS
+    EMAIL_PASS: process.env.EMAIL_PASS,
+    MESSAGE_BROKER_URL: process.env.MESSAGE_BROKER_URL,
+    EXCHNAGE_NAME: process.env.EXCHNAGE_NAME,
+    REMINDER_BINDING_KEY: process.env.REMINDER_BINDING_KEY
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const {PORT} = require('./config/serverConfig');
+const { createChannel} = require('./utils/messageQueue');
 
 // const {sendBasicEmail} = require('./services/email-service');
 const TicketController = require('./controllers/ticket-controller')
 
 const jobs = require('./utils/job')
-const setupAndStartServer = () => {
+
+const setupAndStartServer = async () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+ 
 
     app.post('/api/v1/tickets',  TicketController.create)
 
